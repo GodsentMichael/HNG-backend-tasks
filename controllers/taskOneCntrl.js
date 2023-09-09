ded01d6f9b844d9a0bc485a1647c3f5974ea1ce4
Godsent Michael <gentlemotivator@gmail.com>
2023-09-09 21:26:07 +0100

    Update: ISOToString

--- controllers/taskOneCntrl.js
+++ controllers/taskOneCntrl.js
@@ -1,76 +1,53 @@
 // To create the query endpoint for the task one.
+
 exports.getQuery = async (req, res) => {
-  try {
-    const slackName = req.query.slack_name;
-    const track = req.query.track;
-
-    const githubFileURL =
-      "https://github.com/GodsentMichael/HNG-backend-tasks/blob/main/controllers/taskOneCntrl.js";
-    const githubRepoURL =
-      "https://github.com/GodsentMichael/HNG-backend-tasks.git";
-
-    // So first get the current day of the week
-    const daysOfWeek = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    const currentDay = daysOfWeek[new Date().getUTCDay()];
-
-    // Then the current UTC time
-
-    // Get the current UTC time
-const now = new Date();
-now.setMinutes(now.getMinutes() + now.getTimezoneOffset() + 120); // Adjust for UTC+2
-
-// Format UTC time as a string without "T" and "Z"
-const utcTime = now.toISOString()
-console.log("UTCTIME=>", utcTime)
-
-    // const serverTime = new Date();
-    // console.log("serverTime=>", serverTime);
-    // const serverUtcTime = new Date(serverTime.toISOString());
-    // console.log("serverUtcTime=>", serverUtcTime);
-
-    // Calculate the time difference in milliseconds between server time and UTC time
-    // const timeDifference = Math.abs(serverTime - serverUtcTime);
-    // console.log("timeDifference=>", timeDifference);
-
-    const response = {
+    try {
+      const slackName = req.query.slack_name;
+      const track = req.query.track;
+  
+      const githubFileURL =
+        "https://github.com/GodsentMichael/HNG-backend-tasks/blob/main/controllers/taskOneCntrl.js";
+      const githubRepoURL =
+        "https://github.com/GodsentMichael/HNG-backend-tasks.git";
+  
+      // Get the current day of the week
+      const daysOfWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      const currentDay = daysOfWeek[new Date().getUTCDay()];
+  
+      // Get the current UTC time
+      const currentUTCTime = new Date().toISOString();
+      const currentTimeInMilliseconds = Date.parse(currentUTCTime);
+  
+      // Validate UTC time within +/-2 hours (i.e., 7,200,000 milliseconds)
+      const allowedRange = 2 * 60 * 60 * 1000; // 2 hours in milliseconds
+      const isCurrentTimeWithinRange = (currentTimeInMilliseconds - allowedRange) <= currentTimeInMilliseconds <= (currentTimeInMilliseconds + allowedRange);
+  
+      if (!isCurrentTimeWithinRange) {
+        throw new Error('Current time is not within the allowed range of +/-2 hours');
+      }
+  
+      const response = {
         slack_name: slackName,
         current_day: currentDay,
-        utc_time: utcTime,
+        utc_time: currentUTCTime,
         track: track,
         github_file_url: githubFileURL,
         github_repo_url: githubRepoURL,
         status_code: 200,
       };
-
-      return res.status(200).json(response)
-
-    // So to Validate UTC time within +/-2 minutes i.e (120,000 milliseconds)
-    // if (timeDifference <= 120000) {
-    //   const utcTime = serverTime.toISOString();
-    //   const response = {
-    //     slack_name: slackName,
-    //     current_day: currentDay,
-    //     utc_time: utcTime,
-    //     track: track,
-    //     github_file_url: githubFileURL,
-    //     github_repo_url: githubRepoURL,
-    //     status_code: 200,
-    //   };
-    //   return res.status(200).json(response);
-    // } else {
-    //   // Invalid request due to time difference > +/-2 minutes
-    //   res.status(400).json({ error: "Time validation failed" });
-    // }
-  } catch (error) {
-    console.error("Get Query Error:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
+  
+      return res.status(200).json(response);
+    } catch (error) {
+      console.error("Get Query Error:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  };
+  
